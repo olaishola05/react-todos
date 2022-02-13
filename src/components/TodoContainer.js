f79ba675aced0cd24e725a44108bdc51f8441eea
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-02-13 15:10:52 +0100

    Update UI and handle input checked

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import TodosLists from './TodoLists';
 
 export class TodoContainer extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
   render() {
     return (
       <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
+        <Header />
+        <TodosLists todos={this.state.todos} handleChangeProps={this.handleChange} />
       </div>
     );
   }

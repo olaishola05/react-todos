4ddf189457aad9925d629850caf2a39f2a680875
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-02-16 11:31:17 +0100

    Updates

---
+++ precache-manifest.447299402e9bce28b1147c62269aaf7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263b41b5a02cb3b255ee8ae26e5f8012",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "8722f210152fafe29c24",
+    "url": "/react-todos/static/css/main.58980cbd.chunk.css"
+  },
+  {
+    "revision": "57711e637c0b81316aac",
+    "url": "/react-todos/static/js/2.67ed68e7.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/react-todos/static/js/2.67ed68e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebd6095f3c63574c770",
+    "url": "/react-todos/static/js/3.9390d5ef.chunk.js"
+  },
+  {
+    "revision": "8722f210152fafe29c24",
+    "url": "/react-todos/static/js/main.a4d3dfb4.chunk.js"
+  },
+  {
+    "revision": "d5be9609e95c92751a4f",
+    "url": "/react-todos/static/js/runtime-main.7182c965.js"
+  }
+]);
